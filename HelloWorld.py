4a66d1ee33c30786fa18cc7f1760f7bf9ad20a7f
Tomi <tothtamas0611@gmail.com>
2025-12-03 15:15:50 +0100

    feat: új szöveg az endpoint válaszában

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Ez egy egyszerű DevOps demo alkalmazás!"
+    return "Frissített üzenet: új fejlesztés a feature branch-en!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
